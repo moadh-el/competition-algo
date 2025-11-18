5de5bd2d3cb63f64f1d4653e7b34eb03529a0518
Alexis-Miras <60391553+Alexis-Miras@users.noreply.github.com>
2025-11-18 15:49:06 +0100

    Update Hermione.c

--- Hermione.c
+++ Hermione.c
@@ -25,7 +25,32 @@ int main()
     int D = parse_int(*(first_multiple_input + 0));
 
     int S = parse_int(*(first_multiple_input + 1));
+    
+    if ((S>9*D) || (S == 0 && D > 1)) {
+        return -1;
+    }
+    
+    char* digits = malloc(sizeof(char)*(D + 1));
+    
+    digits[D] = '\0';
+    
+    S = S - 1;
+    for (int i=D-1;i>0;i--) {
+        if (S>9) {
+            digits[i] = '9';
+            S = S-9;
+        } else {
+            digits[i] = S + '0';
+            S = 0;
+        }
+    }
+    
+    digits[0] = 1 + S + '0';
 
+    printf("%s\n", digits);
+    
+    free(digits);
+    
     return 0;
 }
 
