b039462c2f3169b336501b4e884ed641bd3e8cd3
Alexis-Miras <60391553+Alexis-Miras@users.noreply.github.com>
2025-11-19 16:43:45 +0100

    Refactor main function and remove unused code

--- Hermione.c
+++ Hermione.c
@@ -1,25 +1,3 @@
-#include <assert.h>
-#include <ctype.h>
-#include <limits.h>
-#include <math.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-char* readline();
-char* ltrim(char*);
-char* rtrim(char*);
-char** split_string(char*);
-
-int parse_int(char*);
-
-
-
-int main()
-{
     char** first_multiple_input = split_string(rtrim(readline()));
 
     int D = parse_int(*(first_multiple_input + 0));
@@ -27,15 +5,14 @@ int main()
     int S = parse_int(*(first_multiple_input + 1));
     
     if ((S>9*D) || (S == 0 && D > 1)) {
-        return -1;
+        printf("-1\n");
+        return 0;
     }
     
     char* digits = malloc(sizeof(char)*(D + 1));
     
-    digits[D] = '\0';
-    
     S = S - 1;
-    for (int i=D-1;i>0;i--) {
+    for (int i=D-1;i>=0;i--) {
         if (S>9) {
             digits[i] = '9';
             S = S-9;
@@ -45,131 +22,12 @@ int main()
         }
     }
     
-    digits[0] = 1 + S + '0';
+    digits[0] = digits[0]+1;
+    
+    digits[D] = '\0';
 
     printf("%s\n", digits);
     
     free(digits);
     
     return 0;
-}
-
-char* readline() {
-    size_t alloc_length = 1024;
-    size_t data_length = 0;
-
-    char* data = malloc(alloc_length);
-
-    while (true) {
-        char* cursor = data + data_length;
-        char* line = fgets(cursor, alloc_length - data_length, stdin);
-
-        if (!line) {
-            break;
-        }
-
-        data_length += strlen(cursor);
-
-        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
-            break;
-        }
-
-        alloc_length <<= 1;
-
-        data = realloc(data, alloc_length);
-
-        if (!data) {
-            data = '\0';
-
-            break;
-        }
-    }
-
-    if (data[data_length - 1] == '\n') {
-        data[data_length - 1] = '\0';
-
-        data = realloc(data, data_length);
-
-        if (!data) {
-            data = '\0';
-        }
-    } else {
-        data = realloc(data, data_length + 1);
-
-        if (!data) {
-            data = '\0';
-        } else {
-            data[data_length] = '\0';
-        }
-    }
-
-    return data;
-}
-
-char* ltrim(char* str) {
-    if (!str) {
-        return '\0';
-    }
-
-    if (!*str) {
-        return str;
-    }
-
-    while (*str != '\0' && isspace(*str)) {
-        str++;
-    }
-
-    return str;
-}
-
-char* rtrim(char* str) {
-    if (!str) {
-        return '\0';
-    }
-
-    if (!*str) {
-        return str;
-    }
-
-    char* end = str + strlen(str) - 1;
-
-    while (end >= str && isspace(*end)) {
-        end--;
-    }
-
-    *(end + 1) = '\0';
-
-    return str;
-}
-
-char** split_string(char* str) {
-    char** splits = NULL;
-    char* token = strtok(str, " ");
-
-    int spaces = 0;
-
-    while (token) {
-        splits = realloc(splits, sizeof(char*) * ++spaces);
-
-        if (!splits) {
-            return splits;
-        }
-
-        splits[spaces - 1] = token;
-
-        token = strtok(NULL, " ");
-    }
-
-    return splits;
-}
-
-int parse_int(char* str) {
-    char* endptr;
-    int value = strtol(str, &endptr, 10);
-
-    if (endptr == str || *endptr != '\0') {
-        exit(EXIT_FAILURE);
-    }
-
-    return value;
-}
